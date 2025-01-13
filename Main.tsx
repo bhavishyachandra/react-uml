d2b8e218df6b8f484f40709ab6ffcc74b9964109
bhavishyachandra <bhavishyachandra@gmail.com>
2025-01-13 06:05:04 -0500

    add Main component with Hello World message

---
+++ Main.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import App from './App';
+
+const Main = () => {
+    return (
+        <div>
+            <App />
+            <div>
+                Hello World
+            </div>
+        </div>
+    );
+};
+
+export default Main;
