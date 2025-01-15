7cfbaffa32b0e081a3d4fe5780cfa435ba5e8861
bhavishyachandra <bhavishyachandra@gmail.com>
2025-01-15 07:01:16 -0500

    fix: delay Apollon editor destruction to ensure cleanup occurs after rendering

--- App2.tsx
+++ App2.tsx
@@ -26,7 +26,9 @@ const App2 = () => {
             setEditor(apollonEditor);
 
             return () => {
-                apollonEditor.destroy();
+                setTimeout(() => {
+                    apollonEditor.destroy();
+                }, 0);
             };
         }
     }, [diagramData]);
